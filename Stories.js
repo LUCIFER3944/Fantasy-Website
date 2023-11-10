b38f83fca535a35580ad03309a579ec2dc918827
Lucifer <vivekrai3944@gmail.com>
2023-11-10 13:00:34 +0530

    update 2

--- Stories.js
+++ Stories.js
@@ -1,1 +1,8 @@
-
+  // Get the logo element by its ID
+  var logo = document.getElementById("logo1");
+      
+  // Add a click event listener to the logo element
+  logo.addEventListener("click", function() {
+    // Redirect to the new HTML page when the logo is clicked
+    window.location.href = "Html.html";
+  });
